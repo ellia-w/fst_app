f07c9107255f9ffcbc66730c6cfb4c0ac8c149b5
ellia-w <harage2@gmail.com>
2022-08-14 09:34:53 +0300

    adding the frst program

---
+++ FirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿/* It's a simple program on C#
+ */
+
+using System;
+
+class Example
+{
+    //method main
+    static void Main()
+    {
+        Console.WriteLine("simple program on C4+.");
+    }
+
+}
